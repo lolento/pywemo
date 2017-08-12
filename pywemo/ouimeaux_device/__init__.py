66603944b1250a586365672abe092541a87485c2
lolento <hilman.chung@gmail.com>
2017-08-11 21:05:55 -0700

    Update __init__.py

--- pywemo/ouimeaux_device/__init__.py
+++ pywemo/ouimeaux_device/__init__.py
@@ -14,7 +14,7 @@ from .api.xsd import device as deviceParser
 log = logging.getLogger(__name__)
 
 # Start with the most commonly used port
-PROBE_PORTS = (49153, 49152, 49154)
+PROBE_PORTS = (49153, 49152, 49154, 49155, 49156, 49157)
 
 
 def probe_wemo(host):
